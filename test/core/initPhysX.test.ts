7546ae3498fc9cc2b3ae552816fa5dce4d484291
HexaField <joshfield999@gmail.com>
2021-05-23 15:28:23 +1000

    remove uneccesary async

--- test/core/initPhysX.test.ts
+++ test/core/initPhysX.test.ts
@@ -4,14 +4,14 @@ import { setupMain } from '../setupMain'
 import { setupWorker } from '../setupWorker'
 
 describe(("Load PhysX"), () => {
-  it('Loads PhysX', async () => {
-    return await setupMain().then(() => {
+  it('Loads PhysX', () => {
+    return setupMain().then(() => {
       assert(PhysX.PX_PHYSICS_VERSION, 67174656)
     })
   })
 
-  it('Loads PhysX in worker', async () => {
-    return await setupWorker().then((success: boolean) => {
+  it('Loads PhysX in worker', () => {
+    return setupWorker().then((success: boolean) => {
       assert(success, true)
     })
   })
