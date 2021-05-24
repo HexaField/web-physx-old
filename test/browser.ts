eaca37f9497f52814bfa14a959b8c3bd11336ce1
HexaField <joshfield999@gmail.com>
2021-05-24 18:00:50 +1000

    improve browser test logging

--- test/browser.ts
+++ test/browser.ts
@@ -1,8 +1,10 @@
 import { setupWorker } from './setupWorker'
-import physx from '../'
 
 setupWorker().then(async (boolean) => {
   const scene = physx.createScene({})
   const bounce = await scene.getBounceThresholdVelocity()
-  console.log(bounce)
+  console.log('TEST 1:', bounce === 2)
+  scene.setBounceThresholdVelocity(1)
+  const bounce2 = await scene.getBounceThresholdVelocity()
+  console.log('TEST 2:', bounce2 === 1)
 })
